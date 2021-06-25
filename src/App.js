1d28e6ba57c086cfaf6047b73161ba8b0eefd388
cache --timeout=400 <spurvaj@gmail.com>
2021-06-25 23:03:33 +0530

    sigin and signup UI

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ import {
 import Login from './pages/Login';
 import pageNotFound from './pages/404';
 import addEntry from './pages/addEntry';
-import signUp from './pages/signup';
+import SignUp from './pages/signup';
 import dashboard from './pages/dashboard';
 class App extends Component{
   render(){
     return <Router>
       <Switch>
         <Route exact path="/" component={Login}/>
-        <Route exact path="/signup" component={signUp}/>
+        <Route exact path="/signup" component={SignUp}/>
         <Route exact path="/dashboard" component={dashboard}/>
         <Route exact path="/addEntry" component={addEntry}/>
         <Route exact path="/404" component={pageNotFound}/>
